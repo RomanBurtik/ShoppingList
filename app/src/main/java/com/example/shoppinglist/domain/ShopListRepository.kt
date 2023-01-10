3bc493c0356d2c1d006e82939baca5438ace4a01
RomanBurtik <bromav33@gmail.com>
2023-01-10 22:11:21 +0100

    Creation of basic presentation layer

--- app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -1,9 +1,11 @@
 package com.example.shoppinglist.domain
 
+import androidx.lifecycle.LiveData
+
 interface ShopListRepository {
     fun addShopItem(shopItem: ShopItem)
     fun getShopItem(shopItemId: Int):ShopItem
     fun modifyShopItem(shopItem: ShopItem)
     fun deleteShopItem(shopItem: ShopItem)
-    fun getShopList(): List<ShopItem>
+    fun getShopList(): LiveData<List<ShopItem>>
 }
---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.shoppinglist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.*
+
+class MainViewModel: ViewModel() {
+
+    private val repository=ShopListRepositoryImpl
+
+    private val getShopListUseCase=GetShopListUseCase(repository)
+    private val deleteFromShopListUseCase=DeleteFromShopListUseCase(repository)
+    private val addItemToShopListUseCase=AddItemToShopListUseCase(repository)
+    private val getItemInShopListUseCase=GetItemInShopListUseCase(repository)
+    private val modifyItemInShopListUseCase=ModifyItemInShopListUseCase(repository)
+
+    val shopList=getShopListUseCase.getShopList()
+
+    fun deleteFromShopList(shopItem: ShopItem){
+        deleteFromShopListUseCase.deleteShopItem(shopItem)
+    }
+
+    fun editShopItem(shopItem: ShopItem){
+        val newItem=shopItem.copy(isActive = !shopItem.isActive)
+        modifyItemInShopListUseCase.modifyShopItem(newItem)
+
+    }
+}
